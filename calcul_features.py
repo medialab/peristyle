0b7e3a723770a34d38c26abbacde724dff5e908d
oubine <oubine.perrin@gmail.com>
2019-03-12 10:15:32 +0100

    multoprocessing

--- calcul_features.py
+++ calcul_features.py
@@ -8,26 +8,10 @@ import spacy
 from nltk.tokenize import sent_tokenize
 from ural import LRUTrie
 from statistics import median
+from statistics import mean
 from collections import defaultdict
 from timeit import default_timer as timer
-
-
-HTML=re.compile(r'<[^>]*>')
-VIDE=re.compile(r'[\s\n\t\r]+')
-USELESS=re.compile(r'[\'’`\- ]+')
-
-
-POINT=re.compile(r'[!?.]+')
-PUNCT=re.compile(r'[!?,;:_()\[\]«»—"]|[.]+')
-NOT_CHAR=re.compile(r'[.!?,;:\'\-\s\n\r_()\[\]«»’`/"0-9]+')
-NEGATION=re.compile(r' ne | n[^A-z]| non ')
-SUB=re.compile(r'')
-QUOTE=re.compile(r'[\'"’`«»]')
-BRACKET=re.compile(r'[(){}\[\]]')
-
-PERSON=re.compile(r'Gender=[A-z]+\||Number=[A-z]+\||Person=*\|')
-
-csv.field_size_limit(100000000)
+from multiprocessing import Pool
 
 
 class Timer(object):
@@ -43,11 +27,29 @@ class Timer(object):
         print('%s:' % self.name, self.duration)
 
 
+with Timer('load nlp'):
+    nlp = spacy.load('fr_core_news_sm', disable=['textcat', 'parser'])
+
+
+HTML=re.compile(r'<[^>]*>')
+VIDE=re.compile(r'[\s\n\t\r]+')
+USELESS=re.compile(r'[\'’`\- ]+')
+
+POINT=re.compile(r'[!?.]+')
+PUNCT=re.compile(r'[!?,;:_()\[\]«»—"]|[.]+')
+NOT_CHAR=re.compile(r'[.!?,;:\'\-\s\n\r_()\[\]«»’`/"0-9]+')
+CHAR=re.compile(r'[a-zéèà]')
+NEGATION=re.compile(r'\b[Nn][e\'on ]{1,2}\b')
+SUBJ=re.compile(r'\b[Jj]e\b|\b[Mm][aeons]{1,2}\b|\b[JjMm]\'')# + mien mienne
+QUOTE=re.compile(r'["«»]')
+BRACKET=re.compile(r'[(){}\[\]]')
+
+PERSON=re.compile(r'Gender=[A-z]+\||Number=[A-z]+\||Person=*\|')
+
+csv.field_size_limit(100000000)
+
 def is_char(word):
     return not bool(NOT_CHAR.match(word))
-
-
-
 
 def squeeze(txt): #pour être sur que le sent tokenizer de nltk marche bien
     txt=re.sub(r'[.]+', '.', txt)
@@ -60,13 +62,12 @@ def clean(txt):
     return txt
 
 
-
 def generate_sources():
 
     f2=open('sources.csv', 'r')
     sources=csv.DictReader(f2)
 
-    sources_list=[]
+    sources_list=set()
 
     for row in sources:
         source={'url': row['url'],
@@ -77,31 +78,22 @@ def generate_sources():
                 'level1':row['level1_title'],
                 'level2':row['level2_title'],
                 'webentity':row['webentity']}
-        sources_list.append(source)
+        sources_list.add(source)
 
     f2.close()
 
     return sources_list
 
 def generate_stopwords():
-
+    stopwords=set()
     with open ('stopwords_français.txt', 'r') as f:
-        stopwords=f.readlines()
-
-    for i in range (0,len(stopwords)):
-        stopwords[i]=stopwords[i].strip()
-
+        stopwords={word.strip() for word in f.readlines()}
     return stopwords
 
-
 def generate_dictionary():
-
+    dictionary=set()
     with open ('french.txt', 'r') as f:
-        dictionary=f.readlines()
-
-    for i in range (0,len(dictionary)):
-        dictionary[i]=dictionary[i].strip()
-
+        dictionary={word.strip() for word in f.readlines()}
     return dictionary
 
 
@@ -173,28 +165,38 @@ def calcul_verb(verbs, nb_verbs):
 
 def calcul_punct(punctuations, nb_puncts):
 
-    punctuation_diversity=len(punctuations)
     nb_sent=0
     nb_quote=0
-    nb_braket=0
+    nb_bracket=0
 
     result={}
 
     for punct in punctuations.keys():
         if POINT.match(punct):
-            nb_sent+=1
-        elif QUOTE.match(punct):#je compare ça à quoi?
-            nb_quote+=1
+            nb_sent+=punctuations[punct]
+        elif QUOTE.match(punct):
+            nb_quote+=punctuations[punct]
         elif BRACKET.match(punct):
-            nb_braket+=1
+            nb_bracket+=punctuations[punct]
 
+    if nb_quote%2!=0 or nb_bracket%2!=0:
+        if nb_quote%2!=0:
+            nb_quote+=1
+        else:
+            nb_bracket+=1
+    else:
+        nb_quote=nb_quote/2
+        nb_bracket=nb_bracket/2
 
     result['question_prop']=(punctuations['?']/nb_sent)*100
     result['exclamative_prop']=(punctuations['!']/nb_sent)*100
+    result['quote_prop']=(nb_quote/nb_sent)*100
+    result['bracket_prop']=(nb_bracket/nb_sent)*100
 
     return result
 
 def calcul_pos(pos_counting, nb_tokens):
+
     result={}
 
     result['noun_prop']=(pos_counting['NOUN']/nb_tokens)*100
@@ -207,13 +209,10 @@ def calcul_pos(pos_counting, nb_tokens):
 
 #def calcul_ner(entities):
 
-
-
 def pos_tagging(txt): #calcule certaines features en utilisant le pos tagging : temps (cardinalité temps), pos, punct, negation(marche pas top)
 
+    txt=str(txt)
     with Timer('nlp'):
-        nlp = spacy.load('fr_core_news_sm', disable=['textcat', 'parser'])
-        txt=str(txt)
         txt = nlp(txt)
 
     pos_counting=defaultdict(int)
@@ -235,7 +234,13 @@ def pos_tagging(txt): #calcule certaines features en utilisant le pos tagging : 
     for ent in txt.ents:
         entities[ent.label_].add(ent.text)
 
-    verbs_results=calcul_verb(verbs, pos_counting['VERB']+pos_counting['AUX'])
+    if verbs:
+        nb_verbs=pos_counting['VERB']+pos_counting['AUX']
+        verbs_results=calcul_verb(verbs, nb_verbs)
+    else:
+        verbs_results={}
+
+
     punctuation_results=calcul_punct(punctuations, pos_counting['PUNCT'])
     pos_results=calcul_pos(pos_counting,len(txt))
     #ner_results=calcul_ner(entities)
@@ -248,50 +253,51 @@ def pos_tagging(txt): #calcule certaines features en utilisant le pos tagging : 
     return results
 
 
-def count_negation(txt): #semble mieux marcher que le pos tagging
+## Pour les trucs avec re.match
+
+def count_subjectivity(txt, nb_sent):
+    subjectivities=re.findall(SUBJ, txt)
+    return {'subjectivity_prop':(len(subjectivities)/nb_sent)*100}
+
+def count_negation(txt, nb_sent): #mayday: la double négation
     negations=re.findall(NEGATION, txt)
-    return {'nb_negation':len(negations)}
+    return {'prop_negation':(len(negations)/nb_sent)*100}
 
-def tokenize(txt):
+##Pour utiliser NLTK
 
-    txt=squeeze(txt)
-    sentences=nltk.tokenize.sent_tokenize(txt, language='french')
-
-    for i in range(0, len(sentences)):
-        sentences[i]=nltk.word_tokenize(sentences[i])
-
-    return(sentences)
-
-def count_letters(nb_letters, word):
+def count_letters(letters, word):
 
     for char in word:
-        if is_char(char):
-            nb_letters[char]+=1
-    return nb_letters
+        if CHAR.match(char):
+            letters[char]+=1
 
-def calcul_letters(letters, nb_char): #calcul la moyenne d'apparition d'une lettre dans le txt
+    return letters
+
+def calcul_letters(letters, nb_char):
+
+    nb_missing_letters=29-len(letters)
+    for i in range(nb_missing_letters):
+        letters[i]=0
 
     mean_occ_letter=0
-
     for nb_occ_letter in letters.values():
         mean_occ_letter+=nb_occ_letter
 
-    mean_occ_letter=mean_occ_letter/31
+
+    mean_occ_letter=mean_occ_letter/29
     median_occ_letter=median(letters.values())
 
-
-
     return {
-            'letters':letters,
             'mean_occ_letter':mean_occ_letter,
             'median_occ_letter':median_occ_letter
             }
 
 
+def calcul_ARI(txt, stopwords, dictionary): # on pourrait faire tout dans l'un à condition d'abandonner la médiane du nb de mots par phrase
 
-def calcul_ARI(txt, stopwords, dictionary): #on pourrait faire tout dans l'un à condition d'abandonner la médiane du nb de mots par phrase
-
-    txt=tokenize(txt)
+    txt=squeeze(txt)
+    sentences=nltk.tokenize.sent_tokenize(txt, language='french')
+    txt=[nltk.word_tokenize(sentence) for sentence in sentences]
 
     ARI=0.0
     nb_word=0
@@ -334,13 +340,12 @@ def calcul_ARI(txt, stopwords, dictionary): #on pourrait faire tout dans l'un à
             nb_ws.append(len(sentence))
 
 
-    results_letters=calcul_letters(letters, nb_char)
-
     mean_cw=nb_char/nb_word
     mean_ws=nb_word/nb_sentence
 
     median_cw=median(nb_cw)
     median_ws=median(nb_ws)
+
 
     prop_shortword=(nb_shortwords/nb_word)*100
     prop_dictwords=(nb_dictwords/nb_word)*100
@@ -359,29 +364,47 @@ def calcul_ARI(txt, stopwords, dictionary): #on pourrait faire tout dans l'un à
             'mean_ws':mean_ws,
             'median_cw':median_cw,
             'median_ws':median_ws,
-            'nb_shortwords':nb_shortwords,
+            'max_len_word':max(nb_cw),
             'prop_shortwords':prop_shortword,
             'prop_dictwords':prop_dictwords,
-            'voc cardinality':(len(voc)/nb_word)*100
+            'voc_cardinality':(len(voc)/nb_word)*100
             }
-        result.update(results_letters)
+        result.update(calcul_letters(letters, nb_char))
         return result
     else:
         return False
 
 
+def calcul_features(path):
 
-def calcul_features(txt):
+    try:
+        with open (path,'r') as ft:
+            txt=ft.readline()
+    except:
+        return False
+
     results={}
-    stopwords=generate_stopwords()
-    dictionary=generate_dictionary()
+
+    try:
+        results.update(calcul_ARI(txt, stopwords, dictionary))
+
+    except:
+        return False
+
     results.update(pos_tagging(txt))
-    results.update(calcul_ARI(txt, stopwords, dictionary))
-    results.update(count_negation(txt))
+    results.update(count_negation(txt,results['nb_sent']))
+    results.update(count_subjectivity(txt, results['nb_sent']))
+
     return results
 
 
 ##### Fonction pour calculer les nouvelles features sur tous les textes et les enregistrées
+
+def generate_path(row):
+    story_id=row['stories_id']
+    return 'sample/'+story_id+'.txt'
+
+
 
 def find_media_info(sources, media_id, info='level0'):
 
@@ -419,37 +442,32 @@ def ajout_info():
 
     fs=open('sample_normalized_sorted.csv', 'r')
     reader=csv.DictReader(fs)
-    reader.fieldnames+=['ARI', 'nb_sent', 'nb_word', 'nb_char', '']#ajouter les nouveaux features
+    reader.fieldnames+=['ARI', 'nb_sent', 'nb_word', 'nb_char', 'mean_cw', 'mean_ws', 'median_cw', 'median_ws', 'prop_shortwords', 'max_len_word', 'prop_dictwords', 'voc_cardinality', 'mean_occ_letter', 'median_occ_letter', 'prop_negation', 'subjectivity_prop', 'verb_prop', 'past_verb_cardinality', 'pres_verb_cardinality', 'fut_verb_cardinality', 'imp_verb_cardinality', 'other_verb_cardinality','past_verb_prop', 'pres_verb_prop', 'fut_verb_prop','imp_verb_prop', 'plur_verb_prop','sing_verb_prop','verbs_diversity','question_prop','exclamative_prop','quote_prop','bracket_prop','noun_prop','cconj_prop','adj_prop','adv_prop']
 
     fd=open('sample_with_features.csv', 'w')
     writer=csv.DictWriter(fd, fieldnames=reader.fieldnames)
     writer.writeheader()
+    times=[]
 
+    generator=((generate_path(row), row) for row in reader)
+    print(generator)
     i=0
     for row in reader:
-
         i+=1
         story_id=row['stories_id']
-        txt_name='sample/'+story_id+'.txt'
 
-        try:
-            with open (txt_name,'r') as ft:
-                txt=ft.readline()
-
-        except:
-            continue
-
-        '''
-        là il faut utiliser toutes les fonctions dans le bon ordre
-        à la fois nltk et spacy pour ne pas utiliser spacy parser
-        très long...
-        '''
-
-        for key in results.keys():
-            row[key]=results_ARI[key]
-
-        writer.writerow(row)
+        print('')
         print(story_id, i)
+
+        with Pool(4) as pool:
+            stopwords=generate_stopwords()
+            dictionary=generate_dictionary()
+            for features in pool.imap_unordered(calcul_features, generator):
+                if features==False:
+                    continue
+                for key in features.keys():
+                    row[key]=features[key]
+                writer.writerow(row)
 
     fd.close()
     fs.close()
@@ -458,27 +476,8 @@ def ajout_info():
 
 
 
-##### ESSAIE SUR UN TEXT #####
-
-with open ('sample/837192958.txt','r') as ft:
-    txt=ft.readline()
+#### Bonjor on travaille ####
 
 
-txt=clean(txt)
 
-#print(count_punctuation(txt))
-#print(count_negation(txt))
-
-with Timer('calcul_features'):
-    features=calcul_features(txt)
-
-
-for feature in features.keys():
-    print(feature, features[feature])
-    print('')
-
-#print(calcul_ARI(txt))
-
-print(txt)
-
-
+ajout_info()
